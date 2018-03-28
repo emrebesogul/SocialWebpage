4a89ce7153a26fbe7574d5b3702ee0a79e9e18a3
johannes <johannes.maendle@hpe.com>
2018-03-28 20:53:09 +0200

    Add roadmap

--- Frontend/socialwebpage/src/Pages/Roadmap.js
+++ Frontend/socialwebpage/src/Pages/Roadmap.js
@@ -23,7 +23,7 @@ class Roadmap extends Component {
 
         this.checkThisSession();
 
-        this.pageTitle = "Roadmap"
+        this.pageTitle = "Social Webpage Home"
         document.title = this.pageTitle;
       }
 
@@ -56,13 +56,57 @@ class Roadmap extends Component {
               <h2>Roadmap for Scope</h2>
 
               <h3>Milestone 1</h3>
-                <span className='date'>
-                  007. Mar. 2018
+                <span className='roadmap-date'>
+                  07. Mar 2018 -23. Mar 2018
                 </span>
+                <p className="roadmap-content">
+                   -  Post text to platform (Show posts in feed and profile) (DONE)  <br/>
+                 -  Wokring registration and login w/o Exception handling (DONE)   <br/>
+	                 -  First UI implementation (DONE)
+                </p>
               <h3>Milestone 2</h3>
+                <span className='roadmap-date'>
+                  24. Mar 2018 - 05. Apr 2018
+                </span>
+                <p className="roadmap-content">
+                  -  Upload pictures to platform (Show pictures ins feed and profile) (DONE) <br/>
+                  -  Like functionality (konstantin) <br/>
+                  -  Profile-specific content (DONE) <br/>
+                  -  Profile-linking (DONE) <br/>
+                  -  Show current user in sidebar & profile (emre) <br/>
+                  -  Roadmap-Page  <br/>
+                	-  Settings (working) (emre)
+                </p>
               <h3>Milestone 3</h3>
+                <span className='roadmap-date'>
+                  06. Apr 2018 - 21. Apr 2018
+                </span>
+                <p className="roadmap-content">
+                  -  Edit posts <br/>
+                  -  Delete posts <br/>
+                  -  Friends <br/>
+                  -  About Us-Page <br/>
+                	-  Add Dropzone confirmation
+                </p>
               <h3>Alpha</h3>
+                <span className='roadmap-date'>
+                  07. Mar 2018
+                </span>
+                <p className="roadmap-content">
+                  -  Notification center (Optional) <br/>
+	                -  Comments
+                </p>
               <h3>Beta</h3>
+                <span className='roadmap-date'>
+                  07. Mar 2018
+                </span>
+                <p className="roadmap-content">
+                  -  Exception handling in Login/regstration <br/>
+                  -  Content management (Optional) <br/>
+                  -  Clean up code <br/>
+                  -  Minify (CSS, JS,) <br/>
+                	-  GZIP
+                </p>
               <h3>Final Release</h3>
 
             </div>
