fc64428badb78f4f48d4f4dd5312cb1c535a0151
johannes <johannes.maendle@hpe.com>
2018-04-26 17:49:13 +0200

    add ports

--- Frontend/socialwebpage/src/API/GET/GetMethods.js
+++ Frontend/socialwebpage/src/API/GET/GetMethods.js
@@ -2,7 +2,7 @@ import $ from 'jquery';
 import { read_cookie, delete_cookie } from 'sfcookies';
 
 var getUrl = window.location;
-var url = getUrl.protocol + "//" + getUrl.hostname + "/rest";
+var url = getUrl.protocol + "//" + getUrl.hostname + ":8000/rest";
 
 export const getUserData=(api) => {
   return new Promise((resolve, reject) => {
