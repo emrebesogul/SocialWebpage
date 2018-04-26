3a8a6b9c53bf4047032c934c9025e8a2dd2216d8
johannes <johannes.maendle@hpe.com>
2018-04-26 13:35:03 +0200

    red buttons

--- Frontend/socialwebpage/src/Pages/Feed.js
+++ Frontend/socialwebpage/src/Pages/Feed.js
@@ -458,7 +458,7 @@ async handleDeletePost(event, data) {
                                                 <Button className="button-styles mobile-button-border" onClick={((e) => this.confirmFriendRequest(e, item))}>Confirm</Button>
                                               </div>
                                               <div className="flex-item">
-                                                <Button className=" button-styles delete-friend-button mobile-button-border red-border-button" onClick={((e) => this.declineFriendRequest(e, item))}>Decline</Button>
+                                                <Button id="red-border-button" className=" button-styles delete-friend-button mobile-button-border" onClick={((e) => this.declineFriendRequest(e, item))}>Decline</Button>
                                               </div>
                                             </List.Content>
                                           </List.Item>
@@ -485,7 +485,7 @@ async handleDeletePost(event, data) {
 
                                             <List.Content className="flex">
                                               <div className="flex-item">
-                                                <Button className="button-styles delete-friend-button mobile-button-border red-border-button " onClick={((e) => this.deleteFriend(e, item))}>Delete Friend</Button>
+                                                <Button id="red-border-button" className="button-styles delete-friend-button mobile-button-border" onClick={((e) => this.deleteFriend(e, item))}>Delete Friend</Button>
                                                 </div>
                                             </List.Content>
                                           </List.Item>
